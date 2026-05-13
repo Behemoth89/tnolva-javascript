44b00e1cb052c4ce86ef46a8dcc42cc5a6bc76ac
Taavi Nõlvak <tnolva@taltech.ee>
2026-05-13 18:35:22 +0300

    fix swagger

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -38,7 +38,11 @@ const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocs, {
+  swaggerUrl: '/api-docs.json',
+  customCss: '.swagger-ui .topbar { display: none }'
+}));
 
 const PORT = process.env.PORT || 3000;
 
