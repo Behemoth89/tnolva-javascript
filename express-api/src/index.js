ca677c3f2667d789243f52e0618e640abba5d59f
Taavi Nõlvak <tnolva@taltech.ee>
2026-05-13 19:15:39 +0300

    Use swagger-ui bundle with standalone preset

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -17,22 +17,33 @@ const swaggerHtml = `<!DOCTYPE html>
 <html>
 <head>
   <title>API Documentation</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui@4.15.5/dist/swagger-ui.css">
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css">
 </head>
 <body>
   <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui@4.15.5/dist/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" charset="UTF-8"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js" charset="UTF-8"></script>
   <script>
-    const spec = ${JSON.stringify(swaggerSpec)};
-    window.onload = () => {
-      window.ui = SwaggerUIBundle({
-        spec: spec,
+    window.onload = function() {
+      const ui = SwaggerUIBundle({
+        url: '/api.json',
         dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIBundle.SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
       });
+      window.ui = ui;
     };
   </script>
 </body>
 </html>`;
+
+app.get('/api.json', (req, res) => {
+  res.json(swaggerSpec);
+});
 
 app.get('/', (req, res) => {
   res.send(swaggerHtml);
